c75333e59c2566ff6c1937aa7b4f4ad30d194dd7
peter <pngugi.wainaina@gmail.com>
2023-10-08 17:03:13 +0300

    scope lab done

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Write your solution in this file!
+var customerName= "bob"
+function upperCaseCustomerName(){
+return customerName=customerName.toUpperCase();
+}
+function setBestCustomer(){
+    return bestCustomer = 'not bob';
+
+}
+function overwriteBestCustomer(){
+     return bestCustomer="maybe bob";
+}
+const leastFavoriteCustomer="pitt"
+function changeLeastFavoriteCustomer(){
+    leastFavoriteCustomer="kioko"
+}
